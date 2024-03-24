33e529258ff9d8996f30dc87e260118d703fad29
João Victor Porto Fernandes <98399932+Joao-vpf@users.noreply.github.com>
2024-03-24 13:32:48 -0300

    Update bibli.h

--- TCP/bibli.h
+++ TCP/bibli.h
@@ -7,6 +7,7 @@ typedef int_fast64_t ill;
 
 ill inf= 10e16; //Limite do inf
 int anos = 20000; //Quantidade de anos
-
+int max_genes = 4000; //Quantidade maxima de genes
+int tx_gemeos = 100; //Taxa de cada população para tirar os gemeos
 
 using namespace std;
