0b3c62da9b8a2d6752df9750869b9dce869c551a
João Victor Porto Fernandes <98399932+Joao-vpf@users.noreply.github.com>
2024-03-25 16:52:31 -0300

    Add files via upload

---
+++ TSP/bibli.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <bits/stdc++.h>
+
+#define input "C:/Users/PC/Documents/Codigos/Varias coisas/TCP/input.txt"
+
+typedef int_fast64_t ill;
+
+ill inf= 10e16; //Limite do inf
+int anos = 20000; //Quantidade de anos
+int max_genes = 4000; //Quantidade maxima de genes
+int tx_gemeos = 100; //Taxa de cada população para tirar os gemeos
+
+using namespace std;
