32236b8b31558be28e0130b4b818c15e96e3c579
Portin <joaovictorpf719@gmail.com>
2024-06-14 19:49:56 -0300

    1.4B

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from library import read
 from TabuSearch.basic import Basic as tabu
 from ACO.optimized import Optimized as aco
+from multiprocessing import freeze_support
 
-dic, cid = read("dataset/qa194.tsp")
 
-a = aco(ants=100, multiprocess=1, time_max=60)
+if __name__ == '__main__':
+    freeze_support()
+    
+    dic, cid = read("dataset/qa194.tsp")
+    
+    a = aco(ants=100, multiprocess=1, time_max=1)
 
-print(a.run())
+    print(a.run())
